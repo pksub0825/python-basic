3a0123275aa3ffff28ac25add7d0f9eb8fc8dbf1
sub <pksub0825@gmail.com>
2021-05-13 22:08:18 +0900

    20210513

--- practice.py
+++ practice.py
@@ -111,6 +111,37 @@ print(str(station+p))
 # print(ceil(3.14)) #올림.4
 # print(sqrt(16)) #제곱근.4
 
-print("A")
+#랜덤함수
+#from random import *
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random()*10) #0.0~10.0 미만의 임의의 값 생성
+# print(int(random()*10)) # 0~10 미만의 임의의 값 생성
+# print(int(random()*10))
+# print(int(random()*10))
+# print(int(random()*10)+1)# 0~10이하의 임의의 값 생성
 
+#로또번호 생성/ 1~45이하의 임의의 값 생성
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
 
+#print(randrange(1,46)) #1~46미만의 임의의 값 생성 / 뒷숫자 포함하지 않음(뒷숫자-1)
+
+#print(randint(1,45)) #1~45이하의 임의의 값을 생성 / 뒷숫자를 포함함
+
+# Q)당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+#월 4회 스터디를 하는데 1번 오프라인의 날짜를 랜덤으로 정하여라
+
+# 조건 : 랜덤으로 날짜를 뽑아야함
+# 조건2: 월별 날짜는 다름을 감안하여 최소 일수인 28일 이내로 정함
+# 조건3: 매우러 1~3일은 스터티 준비를 해야하므로 제외
+
+# (출력문 예제)
+# 오프라인 스터디모임 날짜는 매월 X일로 선정되었습니다.
+
+from random import *
+d=randint(4,28)
+print("오프라인 스터디모임 날짜는 매월 "+str(d)+"일로 선정되었습니다.")
