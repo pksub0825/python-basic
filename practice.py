863cac334eb1b4233ce3144db76f76bb11072674
sub <pksub0825@gmail.com>
2021-05-13 18:23:45 +0900

    2021.05.13

--- practice.py
+++ practice.py
@@ -99,14 +99,18 @@ print(str(station+p))
 # print(number)
 
 #숫자처리함수
-print(abs(-5)) # 절대값 5 
-print(pow(4,2)) # 4^2=16
-print(max(5,12)) #큰값 12
-print(min(5,12)) #작은값 5
-print(round(3.14)) #반올림 3
-print(round(4.9)) #5
+# print(abs(-5)) # 절대값 5 
+# print(pow(4,2)) # 4^2=16
+# print(max(5,12)) #큰값 12
+# print(min(5,12)) #작은값 5
+# print(round(3.14)) #반올림 3
+# print(round(4.9)) #5
 
-from math import *
-print(floor(4.99)) #내림.4
-print(ceil(3.14)) #올림.4
-print(sqrt(16)) #제곱근.4
+# from math import *
+# print(floor(4.99)) #내림.4
+# print(ceil(3.14)) #올림.4
+# print(sqrt(16)) #제곱근.4
+
+print("A")
+
+
