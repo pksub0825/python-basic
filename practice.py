6986dad406037d9a6df19585f119f69bb77d1db0
sub <pksub0825@gmail.com>
2021-05-24 16:06:28 +0900

    20210524

--- practice.py
+++ practice.py
@@ -254,10 +254,212 @@ Q) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시
 # r3=r2[:3]+str(len(r2))+str(r2.count("e"))+"!"
 # print(r3)
 
+'''
 url="http://daum.net"
 r1=url.replace("http://","")
 r1=r1[:r1.index(".")]
 password=r1[:3]+str(len(r1))+str(r1.count("e"))+"!"
 print("{0}의 비밀번호는 {1}입니다.".format(url,password))
+'''
 
+#리스트 []
 
+#지하철 칸별로 10명, 20명, 30명...
+# subway1=10
+# subway2=20
+# subway3=30
+
+# subway=[10,20,30]
+# print(subway)
+
+# #20이 몇번째칸에 타고있는가?
+# print(subway.index(20))
+
+# #40이 다음 정류장에서 다음 칸에 탐
+# subway.append(40)
+# print(subway)
+
+# #15를 사이에 넣는다
+# subway.insert(1, 15)
+# print(subway)
+
+# #지하철에 있는 사람을 한명씩 뒤에서 꺼냄
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+#같은 사람이 몇명있는지 확인
+# subway.append(10)
+# print(subway)
+# print(subway.count(10))
+
+# #정렬
+# num_list=[4,2,1,5,3]
+# num_list.sort()
+# print(num_list)
+
+# #순서 뒤집기
+# num_list.reverse()
+# print(num_list)
+
+# #모두지우기
+# num_list.clear()
+# print(num_list)
+
+# #다양한 자료형 함께 사용
+# mix_list=["가",20,True]
+# print(mix_list)
+
+# #리스트 확장
+# num_list=[4,2,1,5,3]
+# num_list.extend(mix_list)
+# print(num_list)
+
+# cabinet={3:"유재석", 100:"김태호"}
+# # print(cabinet[3])
+# # print(cabinet[100])
+
+# # print(cabinet.get(3))
+
+# # # print(cabinet[5]) #오류발생후 프로그램 정지
+# # print(cabinet.get(5)) #None 값 발생후 정지되진않음
+# # print(cabinet.get(5, "사용 가능"))
+# # print("hi")
+
+# print(3 in cabinet) #True
+# print(5 in cabinet) #F
+
+# cabinet = {"A-3":"유재석", "B-3":"김태호"}
+# print(cabinet["A-3"])
+
+# #새손님
+# print(cabinet)
+# cabinet["A-3"]="김종국"
+# cabinet["C-3"]="조세호"
+# print(cabinet)
+
+# #간 손님
+# del cabinet["A-3"]
+# print(cabinet)
+
+# #key들만 출력
+# print(cabinet.keys())
+
+# #value 들만 출력
+# print(cabinet.values())
+
+# #key, value 쌍으로 출력
+# print(cabinet.items())
+
+# #목욕탕 폐점
+# cabinet.clear()
+# print(cabinet)
+
+#튜플
+# menu=("돈까스", "치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+# # menu.add("생선까스") #튜플은 고정된 값만 지원하고, add할수 없음
+
+# # name="김종국"
+# # age=20
+# # hobby="코딩"
+# # print(name, age, hobby)
+
+# (name, age, hobby) = ("김종국", 20, "코딩")
+# print(name, age)
+
+#세트 (집합)
+#중복안됨, 순서없음
+# set={1,2,3,3,3}
+# print(set) #{1,2,3}
+
+# java={"유재석", "김태호", "양세형"}
+# python=set(["유재석", "박명수"])
+
+# #교집합
+# print(java&python)
+# print(java.intersection(python))
+
+# #합집합
+# print(java | python)
+# print(java.union(python))
+
+# #차집합(java는 할수 있지만 python은 모르는 개발자)
+# print(java-python)
+# print(java.difference(python))
+
+# #python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# #자바를 잊었어요
+# java.remove("김태호")
+# print(java)
+
+#자료구조 변경
+#커피숍
+# menu={"커피","우유", "주스"}
+# print(menu, type(menu))
+
+# menu=list(menu)
+# print(menu, type(menu))
+
+# menu=tuple(menu)
+# print(menu, type(menu))
+
+# menu=set(menu)
+# print(menu, type(menu))
+
+'''
+Quiz) 당신의 학교에서는 파이썬 코딩대회를 주최합니다.
+참석률을 높이기 위해 댓글 이벤트를 진행,
+댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하시오,
+
+조건1: 편의상 댓글은 20명이 작성하였고, 아이디는 1~20이라고 가정
+조건2: 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+조건3: random 모듈의 shuffle과 sample을 활용
+
+(출력예제)
+---당첨자발표---
+치킨 당첨자 : 1
+커피 당첨자 : [2,3,4]
+---축하합니다---
+
+# (활용예제)
+from random import  *
+lst=[1,2,3,4,5]
+print(lst)
+shuffle(lst)
+print(lst)
+print(sample(lst,1))
+
+'''
+
+# lst=[1,2,3,4,5,6,7,8,9,10]
+# from random import *
+# shuffle(lst)
+# sample(lst,1)
+# print("---당첨자발표---")
+# print("치킨당첨자 : "+lst)
+
+from random import *
+users=range(1,21) #1부터 20까지 숫자 생성
+# print(type(users))
+users=list(users)
+# print(type(users))
+
+shuffle(users)
+
+winners=sample(users, 4)
+print("---당첨자발표---")
+print("치킨당첨자 : {0}".format(winners[0]))
+print("커피당첨자 : {0}".format(winners[1:]))
+print("---축하합니다.---")     
